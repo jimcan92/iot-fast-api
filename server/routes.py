56c4dfd03f75158e32668b6a90c519407040bdf1
jimcan92 <jimcan051592@gmail.com>
2022-06-28 12:50:56 +0800

    database added

--- server/routes.py
+++ server/routes.py
@@ -8,25 +8,6 @@ from server.db import database, iot_data_table
 
 router = APIRouter()
 
-iotdata = {
-    "1": {
-        "device_id": "esp_01",
-        "bldg_name": "techbldg",
-        "room_name": "rm01",
-        "temp": 23.4,
-        "humd": 76.4,
-        "created_at": datetime.now(),
-    },
-    "2": {
-        "device_id": "esp_01",
-        "bldg_name": "techbldg",
-        "room_name": "rm01",
-        "temp": 34.1,
-        "humd": 68.2,
-        "created_at": datetime.now(),
-    },
-}
-
 
 @router.get("/")
 async def get_iot_data() -> list[IotData]:
